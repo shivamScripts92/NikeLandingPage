3208be212557cc11f4288f469b85a86508ab3342
Shivam <shivamsri406@gmail.com>
2024-08-26 15:57:50 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+function Navigation(){
+return <div> 
+       <nav className="container">
+      <div className="logo"></div>
+         <img src="/images/brand_logo.png" alt="logo"></img>
+
+       <ul>
+         <li href="#">Menu</li>
+         <li href="#">Location</li>
+         <li href="#">About</li>
+         <li href="#">Contact</li>
+
+       </ul>
+
+       <button> Login  </button>
+     </nav>
+
+</div>
+}
+
+export default Navigation;
